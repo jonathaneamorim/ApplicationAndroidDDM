3a4f45f191036f2ab22644a1b243518404818894
jonathaneamorim <jonathaneamorim@gmail.com>
2025-08-20 20:15:17 -0300

    Ciclos de vida

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.myapplication;
 
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,6 +15,36 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        Log.d("Ciclo de vida", "onCreate ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Ciclo de vida", "onStart ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Ciclo de vida", "onStop ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Ciclo de vida", "onRestart ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Ciclo de vida", "onDestroy ");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
